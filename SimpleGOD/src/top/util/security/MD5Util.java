8a506bce0c2319fb19d06f1dc2be1b6f8262baa5
gaoyx <gaoyx1469@163.com>
2019-03-07 12:00:20 +0800

    MD5工具类

---
+++ SimpleGOD/src/top/util/security/MD5Util.java
@@ -0,0 +1,37 @@
+package top.util.security;
+
+import java.security.MessageDigest;
+
+import sun.misc.BASE64Encoder;
+
+/**
+ * MD5工具类，生成MD5码值
+ * 
+ * @author 高宇翔
+ *
+ */
+public class MD5Util {
+
+	/**
+	 * 根据传入字符串，使用MD5算法得到128位散列值，输出其BASE64编码
+	 * 
+	 * @param message
+	 * @return
+	 */
+	public static String generateMD5(String message) {
+		try {
+
+			// 选择md5算法
+			MessageDigest md = MessageDigest.getInstance("md5");
+
+			// 生成二进制串
+			byte b[] = md.digest(message.getBytes());
+
+			// 采用BASE64编码变换字节序列为明文，原理是把3个字节转换为4个字节
+			BASE64Encoder base64 = new BASE64Encoder();
+			return base64.encode(b);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
