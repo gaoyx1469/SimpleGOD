6458c53df6df93e84ccada97cc8fdebd28f1b544
gaoyx <gaoyx1469@163.com>
2019-05-15 17:57:57 +0800

    过滤器

---
+++ SimpleGOD/src/top/trial/servlet/filter/InnerDemoFilter.java
@@ -0,0 +1,34 @@
+package top.trial.servlet.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class InnerDemoFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		//System.out.println("InnerDemoFilter Before");
+		chain.doFilter(request, response);
+		//System.out.println("InnerDemoFilter After");
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		//System.out.println("InnerDemoFilter Init");
+		Filter.super.init(filterConfig);
+	}
+	
+	@Override
+	public void destroy() {
+		//System.out.println("InnerDemoFilter Destroy");
+		Filter.super.destroy();
+	}
+
+}
---
+++ SimpleGOD/src/top/trial/servlet/filter/OuterDemoFilter.java
@@ -0,0 +1,33 @@
+package top.trial.servlet.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class OuterDemoFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		//System.out.println("OuterDemoFilter Before");
+		chain.doFilter(request, response);
+		//System.out.println("OuterDemoFilter After");
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		//System.out.println("OuterDemoFilter Init");
+		Filter.super.init(filterConfig);
+	}
+	
+	@Override
+	public void destroy() {
+		//System.out.println("OuterDemoFilter Destroy");
+		Filter.super.destroy();
+	}
+}
