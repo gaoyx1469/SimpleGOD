eca473445528275bbb5d41380a796a2e54f9aeb3
gaoyx <gaoyx1469@163.com>
2019-05-21 17:09:42 +0800

    时间上传下载

--- src/top/trial/servlet/FileUploadDemoServlet.java
+++ src/top/trial/servlet/FileUploadDemoServlet.java
@@ -5,7 +5,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.List;
+import java.util.UUID;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -29,11 +31,13 @@ public class FileUploadDemoServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		PrintWriter pout = response.getWriter();
 		try {
 			if (ServletFileUpload.isMultipartContent(request)) {
 				DiskFileItemFactory factory = new DiskFileItemFactory();// 默认配置
 				ServletFileUpload sfu = new ServletFileUpload(factory);// 使用默认配置创建核心类
+				sfu.setFileSizeMax(4 * 1024 * 1024);// 设置单文件最大大小4M
+				sfu.setSizeMax(10 * 1024 * 1024);// 设置总文件最大大小10M
 				List<FileItem> items = sfu.parseRequest(request);
 				for (FileItem item : items) {
 					if (item.isFormField()) {// 是普通输入
@@ -41,23 +45,40 @@ public class FileUploadDemoServlet extends HttpServlet {
 						String itemValue = item.getString("UTF-8");// 使用UTF-8编码转换
 						System.out.println(itemName + "====" + itemValue);
 					} else {// 是文件输入
-						InputStream in = item.getInputStream();
-						String fileName = item.getName().substring(item.getName().lastIndexOf("\\") + 1);
-						String filePath = getFilePath(getServletContext().getRealPath("/WEB-INF/files"), fileName);
-						OutputStream out = new FileOutputStream(filePath);
+						// 得到MIME类型
+						String mimeType = item.getContentType();
+						// 只允许上传图片
+						if (mimeType.startsWith("image")) {
+							InputStream in = item.getInputStream();
+							String fileName = item.getName();
+							if (item.getName() == null || "".equals(item.getName().trim())) {// 空文件跳过
+								continue;
+							}
+							fileName = fileName.substring(item.getName().lastIndexOf("\\") + 1);
+							String filePath = getFilePath(getServletContext().getRealPath("/WEB-INF/files"),
+									UUID.randomUUID() + "_" + fileName);// 文件放WEB-INF中，文件名添加UUID并增加两层目录结构
+							OutputStream out = new FileOutputStream(filePath);
 
-						byte[] b = new byte[1024];
-						int len = -1;
-						while ((len = in.read(b)) != -1) {
-							out.write(b);
+							byte[] b = new byte[1024];
+							int len = -1;
+							while ((len = in.read(b)) != -1) {
+								out.write(b, 0, len);
+							}
+							out.close();
+							in.close();
+							item.delete();// 删除临时文件
 						}
-						out.close();
-						in.close();
 					}
 				}
 			} else {
 				System.out.println("代码有问题吧哥哥，快看看Form表单enctype的值");
 			}
+		} catch (org.apache.commons.fileupload.FileUploadBase.FileSizeLimitExceededException e) {
+			// 单个文件超出大小时的异常
+			pout.write("单个文件大小不能超出4M");
+		} catch (org.apache.commons.fileupload.FileUploadBase.SizeLimitExceededException e) {
+			// 总文件超出大小时的异常
+			pout.write("总文件大小不能超出10M");
 		} catch (FileUploadException e) {
 			throw new RuntimeException("服务器忙");
 		}
@@ -65,11 +86,17 @@ public class FileUploadDemoServlet extends HttpServlet {
 	}
 
 	private String getFilePath(String realPath, String fileName) {
-		File file = new File(realPath);
-		if(!file.exists()) {
+
+		int hashCode = fileName.hashCode();
+		int dir1 = hashCode & 0xf;// 0000~1111：整数0~15共16个
+		int dir2 = (hashCode & 0xf0) >> 4;// 0000~1111：整数0~15共16个
+
+		String path = realPath + "\\" + dir1 + "\\" + dir2; // WEB-INF/files/1/12
+		File file = new File(path);
+		if (!file.exists())
 			file.mkdirs();
-		}
-		return realPath + "/" + fileName;
+
+		return path + "\\" + fileName;
 	}
 
 	@Override
