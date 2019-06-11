11049ed42bf195eddb2180e1fca52c7974291fd6
gaoyx <gaoyx1469@163.com>
2019-06-11 09:11:13 +0800

    注解

--- src/top/trial/annotation/AnnotationUnitDemo.java
+++ src/top/trial/annotation/AnnotationUnitDemo.java
@@ -5,8 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 模拟单元测试注解，仅定义超时时间字段
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.METHOD, ElementType.FIELD })
-public @interface AnnotationDemo {
-	String desc();
+public @interface AnnotationUnitDemo {
+	long timeout() default Long.MAX_VALUE;
 }
---
+++ src/top/trial/annotation/UnitCase.java
@@ -0,0 +1,21 @@
+package top.trial.annotation;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+import top.util.properties.PropertiesUtil;
+
+public class UnitCase {
+	
+	/**
+	 * 实现代码的单元测试
+	 * 
+	 * @throws Exception
+	 */
+	@AnnotationUnitDemo(timeout=1)
+	public void testCase() throws Exception {
+		String rootDemo = PropertiesUtil.getPropertiesValueByClassloader("rootDemo.properties", "propertiesName");
+		System.out.println(rootDemo);
+	}
+	
+}
